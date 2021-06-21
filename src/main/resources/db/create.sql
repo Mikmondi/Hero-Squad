ec004544a563011d43caf048029d5fafb9e26b09
Mikmondi <mikmondi@gmail.com>
2021-06-22 00:15:38 +0300

    added homepage image

--- src/main/resources/db/create.sql
+++
@@ -1,8 +0,0 @@
-SET MODE PostgresQl;
-
-CREATE DATABASE hero_squad;
-\c hero_squad;
-CREATE TABLE heroes (id SERIAL PRIMARY KEY, name VARCHAR, age INTEGER,  power VARCHAR,  weakness VARCHAR,  squadId INTEGER);
-
-CREATE TABLE squads ( id SERIAL PRIMARY KEY, name VARCHAR);
-CREATE DATABASE hero_squad_test WITH TEMPLATE hero_squad;
